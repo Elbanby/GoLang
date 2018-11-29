7666368d7d1132e91fbba323ef5feaf8a00acb00
Omar Elbanby <omar.el-banby2@gwl.ca>
2018-11-29 11:52:28 -0500

    Hello World

---
+++ hello/hello.go
@@ -0,0 +1,9 @@
+//which package the file belon gs to,
+package main
+//List of other pacakges that the program imports
+//fmt --> stands for formatted. It is the base IO
+import "fmt"
+
+func main() {
+  fmt.Println("Hello World")
+}
